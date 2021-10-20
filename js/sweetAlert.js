b0839cd927d71888012f3d649e691d44bba9f584
Íkaro Chacal Guerrero <ikarochacalguerrero@gmail.com>
2021-10-20 12:28:26 -0500

    boton alerta transicion arriba abajo

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -1,45 +1,50 @@
 (async()=>{
-    const {value: ruta} = await Swal.fire({
+    // const {value: ruta} = 
+    await Swal.fire({
         html:
             '<strong><p class= "title">XALTILOLLI</p></strong>'+
             
             '<p class= "rojo ">Espacio de artes, memorias y resistencias</p>'+
       
               '<h2 class = "cuentanos ">Cuentanos sobre tu experiencia y vivencia en Tlatelolco<h2>'+
-              '<a href="html/formulario.html"><button >hola</button</a>> <button>hola</button>'
+              '<div class = "contenedor-botones"><a href="html/formulario.html" >'+
+              '<button class = "btn"> <strong<span>Grabar Testimonio</span></strong> </button> </a>'+     
+              '<a href="html/colage_videos.html" >'         
+              +'<button class = "btn"> <strong><span>Ver Testimonios</span></strong></button> </a>'+
+              '<button class ="btn t"><span>Grabar Testimonio</span></span></button>'+
+              '<button class = "btn t"><span>Ver Testimonios</span></button>'+
+              '</div>'
+              
+              
             //  , +
             //   '<button onClick=())=>mostrarAlerta() >Grabar testimonio</button><button>Ver Testimonios</button>'
               ,
             showCloseButton: false,
-            showCancelButton: true,
-            focusConfirm: false,
-            confirmButtonText:
-              '<i class="fa fa-thumbs-up"></i> Grabar Testimonio',
-            confirmButtonAriaLabel: 'Thumbs up, great!',
-            cancelButtonText:
-            '<i class="fa fa-thumbs-up"></i> Ver Testimonios',
-            cancelButtonAriaLabel: 'Thumbs down',
+            showCancelButton: false,
+            showConfirmButton: false,
+            focusConfirm: false,            
             width:'60%',
             allowOutsideClick: false,
-            background:'rgba(255, 255, 255, .8)',
-            // input: 'select',
-            // inputPlaceholder: 'País',
-            // inputValue: '',
-            // inputOptions: {
-            //     mexico: 'Mexico',
-            //     españa: 'España',
-            //     francia: 'Francia'
-            // }
-    } ).then(resultado=>{
-        if(resultado.value){
-            alert('grabar');
-        }else{
-            alert('Ver Testimonios')
-        }
-    })
-    // if(ruta){
-    //     alert(ruta);
-    // }
+            background:'rgba(255, 255, 255, .9)',
+//             // input: 'select',
+//             // inputPlaceholder: 'País',
+//             // inputValue: '',
+//             // inputOptions: {
+//             //     mexico: 'Mexico',
+//             //     españa: 'España',
+//             //     francia: 'Francia'
+//             // }
+    } )
+//     //.then(resultado=>{
+//     //     if(resultado.value){
+//     //         alert('grabar');
+//     //     }else{
+//     //         alert('Ver Testimonios')
+//     //     }
+//     // })
+//     // if(ruta){
+//     //     alert(ruta);
+//     // }
 
 })()
 
