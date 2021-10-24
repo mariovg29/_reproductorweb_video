e5a9f3784df23c735893b658104da3baca2e1d58
Íkaro Chacal Guerrero <ikarochacalguerrero@gmail.com>
2021-10-23 23:18:47 -0500

    temporizador agregado

--- js/capturarVideo.js
+++ js/capturarVideo.js
@@ -15,16 +15,43 @@ const video= document.getElementById('video');
 
 
 let arrvideo=[];
+
+
 function record(stream){
+    $duracion = document.querySelector("#duracion"),
+    
+   
     video.srcObject= stream;
     let mediaRecorder = new MediaRecorder(stream, {
         mimeType: 'video/webm;codecs=h264,vp9,opus'
     });
-    document.getElementById('takevideo').addEventListener("click", ()=>{
-        mediaRecorder.start();
-        setTimeout(()=>mediaRecorder.stop(),5000);
+    var btn= document.getElementById("takevideo");
+    var temp = document.getElementById(" duración2");
+    document.getElementById('takevideo').addEventListener("click", Start);
+
+        function Start(){
+        mediaRecorder.start(),
+       
+        
+        
+        console.log("Started");
+        btn.removeEventListener("click", Start);
+        btn.addEventListener("click", Stop);
+        btn.value = "Stop";
+        let temporizador = new Temporizador('temporizador', 299, 0, 60);
+       temporizador.conteoSegundos();
+       setTimeout(()=>mediaRecorder.stop(), 301000);
+        
+    }
     
-    });
+    function Stop(){
+        mediaRecorder.stop(),
+        console.log("Stopped");
+        btn.removeEventListener("click", Stop);
+        btn.addEventListener("click", Start);
+        btn.value = "start";
+        temp.value="00:00";
+    }
 
     
 
@@ -50,9 +77,50 @@ function record(stream){
         link.style.display = "none";
         document.body.appendChild(link);
         link.click();
-        link.click();
-        
         link.remove();
     }
+    class Temporizador {
+        constructor(id, inicio, fin, contadorsec) {
+
+            this.id = id;
+            this.inicio = inicio;
+            this.fin = fin;
+            this.contador = inicio;
+            this.contadorsec = contadorsec;
+            this.conteoSegundos = function () {
+                if (this.contador == this.fin) {
+                    this.conteoSegundos = null;
+                    return;
+                }
+                let tiempo = this.contador;
+                let min = tiempo / 60;
+                var minutos = Math.trunc(min);
+                var showseconds = "00";
+                var showsecondsreal = this.contadorsec;
+                this.contadorsec--;
+                if (minutos < 10)
+                    minutos = "0" + minutos;
+                if (this.contadorsec < 10)
+                    this.contadorsec = "0" + this.contadorsec;
+                if (this.contadorsec == 60) {
+                    document.getElementById("duracion2").innerHTML = minutos + ":" + showseconds;
+                } else { document.getElementById("duracion2").innerHTML = minutos + ":" + this.contadorsec; }
+                if (this.contadorsec < 1)
+                    this.contadorsec = 60;
+
+
+
+
+
+
+                document.getElementById("duracion").innerHTML = this.contador--;
+                //    document.getElementById("duracion2").innerHTML =minutos+":"+this.contadorsec;
+                setTimeout(this.conteoSegundos.bind(this), 1000);
+
+            };
+
+        }
+    }
+    
 
 }
